862b13888ffd3e1afe12e973f6bd98b8cc49234b
Jonathan Michel <jonathanmichel@bluewin.ch>
2018-06-29 15:24:21 +0200

    Up and ack symmetric tests implmentation

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@
 #include <string>
 #include <sstream>
 #include <pthread.h>
-#include "time.h"
+#include <sys/time.h>
 
 using namespace DJI::OSDK;
 using namespace DJI::OSDK::Telemetry;
@@ -45,19 +45,36 @@ void parseFromMobileCallback(DJI::OSDK::Vehicle*      vehicle,
                                 DJI::OSDK::RecvContainer recvFrame,
                                 DJI::OSDK::UserData      userData);
 
+#define START_CHAR '@'
+#define END_CHAR '#'
+
 void sendData();
 
 void displayMenu();
 int getNumber(std::string message);
+long getTimeMs();
 
 Vehicle*   vehicle;
 
 #define FRAME_TO_SEND 300
 #define FRAME_LENGTH 100
 
-uint8_t frame[FRAME_LENGTH];
-uint16_t frameCounter;
-struct timespec lastTime;
+uint8_t testFrame[FRAME_LENGTH];
+uint16_t ackCounter;
+long startTime;
+long receivedFrames, receivedBytes;
+
+enum MocTestMode {
+    none = 0,
+    down,
+    up,
+    ackUp,
+    ackDown
+};
+MocTestMode testMode;
+void sendModeStartRequest(MocTestMode testMode);
+void sendModeEndRequest(MocTestMode testMode);
+void sendModeRequest(char operation, MocTestMode testMode);
 
 /*! main
  *
@@ -68,24 +85,21 @@ main(int argc, char** argv)
     enum State {
         init,
         loop,
-        end
+        end,
     };
 
-
-    // MOC Frames tests
     bool running = true;
-    int delayMs, frame_length;
-    useconds_t delayUs;
-    uint16_t framesToSend = FRAME_TO_SEND;
+
+    // MOC Frame test
     uint8_t n = 0;
     while (true) {
-        frame[n] = n;
+        testFrame[n] = n;
         n++;
         if (n == FRAME_LENGTH) {
             break;
         }
     }
-    frameCounter = 0;
+    testMode = none;
 
     // Initialize variables
     int functionTimeout = 1;
@@ -107,52 +121,75 @@ main(int argc, char** argv)
     vehicle->moc->setFromMSDKCallback(parseFromMobileCallback);
 
     // Display interactive prompt
-
-
     while(running) {
         displayMenu();
         char inputChar;
+        // Get user choice
         std::cin >> inputChar;
+        // Get newline char
+        std::cin.get();
 
         switch (inputChar) {
-            case 'a':
-                monitoredTakeoff(vehicle);
-                monitoredLanding(vehicle);
-                break;
-            case 'b':
-                monitoredTakeoff(vehicle);
-                moveByPositionOffset(vehicle, 0, 6, 0, 30);
-                moveByPositionOffset(vehicle, 6, 0, 0, -30);
-                moveByPositionOffset(vehicle, -6, -6, 0, 0);
-                monitoredLanding(vehicle);
-                break;
-            case 'd':
-                DSTATUS("Frame counter : %u", frameCounter);
-                frameCounter = 0;
-                break;
-            case 's':
-                sendData();
-                break;
-            case 't':
-                frame_length = getNumber("Chose frame length : ");
-                delayMs = getNumber("Chose delay (ms) : ");
+            case '1': {
+                testMode = down;
+                unsigned int delayMs, frame_length;
+                useconds_t delayUs;
+                // Get parameters
+                frame_length = (unsigned int)getNumber("Frame length (bytes) : ");
+                delayMs = (unsigned int)getNumber("Send delay (ms) : ");
+                delayUs = delayMs * (useconds_t)1000;
+                // Max testFrame length
                 if(frame_length > 100) {
                     frame_length = 100;
                 }
 
-                std::cout << "Down-test running : length = " << frame_length << " bytes, delay = " << delayMs << " ms" << std::endl;
-                std::cout << "..." << std::endl;
-                delayUs = delayMs * (useconds_t)1000;
+                // Send initializing command to ground station
+                DSTATUS("Down test initializing : length = %u bytes, delay = %u ms ...", frame_length, delayMs);
+                sendModeStartRequest(down);
+                usleep(1000000);
 
-                while(framesToSend != 0) {
-                    vehicle->moc->sendDataToMSDK(frame, (uint8_t)frame_length);
-                    DSTATUS("Frame %u sent", FRAME_TO_SEND - framesToSend);
-                    framesToSend--;
+                // Run test
+                DSTATUS("Down test running ...");
+                startTime = getTimeMs();
+                uint16_t framesSent = 0;
+                while(framesSent < FRAME_TO_SEND) {
+                    vehicle->moc->sendDataToMSDK(testFrame, (uint8_t)frame_length);
+                    DSTATUS("Down test : Frame %u sent", framesSent);
+                    framesSent++;
                     usleep(delayUs);
                 }
-                framesToSend = FRAME_TO_SEND;
-                DSTATUS("Down-test ended : %u frames sent", FRAME_TO_SEND);
+                long diffTimeMs = getTimeMs() - startTime;
+                // Remove last delay
+                diffTimeMs -= delayUs / 1000;
+                // End of test
+                usleep(500000);
+                sendModeEndRequest(down);
+                long bytesSent = (long)frame_length * FRAME_TO_SEND;
+                DSTATUS("Down test ended (length = %u bytes, delay = %u ms) : %u frames sent (%ld Bytes) in %ld ms", frame_length, delayMs, FRAME_TO_SEND, bytesSent, diffTimeMs);
+                double dataFlow = bytesSent * 1000 / diffTimeMs;
+                DSTATUS("Data flow = %lf Bytes/s", dataFlow);
+                testMode = none;
+            }
                 break;
+            case '2': {
+                DSTATUS("MOC - Ack down test initialization");
+                sendModeStartRequest(ackDown);
+            }
+                break;
+            case 'r':
+                DSTATUS("Test status reset");
+                sendModeEndRequest(none);
+                testMode = none;
+                break;
+            case 's': {
+                std::cout << "Type command to send : " << std::endl;
+                std::string command;
+                std::getline(std::cin, command);
+                DSTATUS("Send data to mobile : %s", command.c_str());
+                vehicle->moc->sendDataToMSDK((uint8_t *)command.c_str(), command.length());
+            }
+                break;
+
             default:
                 break;
 
@@ -167,19 +204,16 @@ void displayMenu() {
             << "| Available commands:                                            |"
             << std::endl;
     std::cout
-            << "| [a] Monitored Takeoff + Landing                                |"
+            << "| [1] Down test                                                  |"
             << std::endl;
     std::cout
-            << "| [b] Monitored Takeoff + Position Control + Landing             |"
+            << "| [2] Ack down test                                              |"
             << std::endl;
     std::cout
-            << "| [d] Display and reset frame counter                            |"
+            << "| [r] Reset test status                                          |"
             << std::endl;
     std::cout
-            << "| [s] Send \"Hello word from M210Pi3\"                             |"
-            << std::endl;
-    std::cout
-            << "| [t] Test speed                                                 |"
+            << "| [s] Send custom command                                        |"
             << std::endl;
 }
 
@@ -209,33 +243,165 @@ parseFromMobileCallback(DJI::OSDK::Vehicle*      vehicle,
     uint16_t msgLength = recvFrame.recvInfo.len - formatFrameLength;
     uint8_t* data = recvFrame.recvData.raw_ack_array;
 
-    frameCounter++;
+    switch (data[0]) {
+        // New test launched
+        case '@':
+            switch (data[1]) {
+                case up:
+                    testMode = up;
+                    DSTATUS("MOC - Up test launched");
+                    testMode = up;
+                    receivedFrames = 0;
+                    receivedBytes = 0;
+                    sendModeStartRequest(up);
+                    startTime = getTimeMs();
+                    break;
+                case down:
+                    // Down test can not be launch by testFrame
+                    break;
+                case ackUp :
+                    DSTATUS("MOC - Ack up test launched");
+                    testMode = ackUp;
+                    ackCounter = 0;
+                    sendModeStartRequest(ackUp);
+                    break;
+                case ackDown:
+                    testMode = ackDown;
+                    DSTATUS("MOC - Ack down test launched");
+                    receivedFrames = 0;
+                    receivedBytes = 0;
+                    uint8_t b[2];
+                    b[0] = '-';
+                    b[1] = 0;
+                    vehicle->moc->sendDataToMSDK(b, 2);
+                    break;
+                default:
+                    DERROR("MOC - Unknown test launched");
+                    break;
+            }
+            break;
+        // Test ended
+        case '#':
+            switch (data[1]) {
+                // Reset status mode
+                case none:
+                    DSTATUS("MOC - Test mode reset");
+                    testMode = none;
+                    break;
+                case up: {
+                    long diffTime = getTimeMs() - startTime - 1000;   // 1000ms before send of # end character
+                    DSTATUS("MOC - Up test ended : %ld frames received (%ld Bytes) in %ld ms", receivedFrames,
+                            receivedBytes, diffTime);
+                    double dataFlow = receivedBytes * 1000 / diffTime;
+                    DSTATUS("MOC - Up flow = %lf Bytes/s", dataFlow);
+                    testMode = none;
+                }
+                    break;
+                case ackUp:
+                    DSTATUS("MOC - Ack up test ended");
+                    testMode = none;
+                    break;
+                case ackDown:
+                    // No end request are supposed to be received from ground station
+                    DSTATUS("MOC - Ack down test ended ??");
+                    testMode = none;
+                    break;
+                default:
+                    DERROR("MOC - Unknown test ended");
+                    testMode = none;
+                    break;
 
-    if(data[0] == '#') {
-        if(data[1] < 101) {
-            frame[0] = data[1];
-            DSTATUS("%u", data[1]);
-            vehicle->moc->sendDataToMSDK(frame, 100);
-        }
-    } else {
-        struct timespec currentTime;
-        clock_gettime(CLOCK_MONOTONIC_RAW, &currentTime);
+            }
+            break;
+        // Data received
+        default:
+            switch (testMode) {
+                case down:
+                    // No data to received in this mode
+                    break;
+                case up:
+                    // Increment received counter
+                    DSTATUS("MOC - Test up : Data received (%u) : %u", msgLength, receivedFrames);
+                    receivedFrames++;
+                    receivedBytes += msgLength;
+                    break;
+                case ackUp:
+                    // Verify that requested testFrame is correctly numbered
+                    if(data[1] == ackCounter) {
+                        testFrame[0] = '-'; // random char to avoid START_CHAR or END_CHAR
+                        testFrame[1] = data[1];
+                        DSTATUS("MOC - Ack up test send testFrame %u", data[1]);
+                        vehicle->moc->sendDataToMSDK(testFrame, 100);
+                        ackCounter++;
+                    } else {
+                        DSTATUS("MOC - Ack up test testFrame counter error");
+                        testMode = none;
+                    }
+                    break;
+                case ackDown: {
+                    bool sendData = true;
+                    uint8_t index = data[1];
+                    DSTATUS("MOC - Ack down test : Frame %u received (%u bytes)", index, msgLength);
+                    receivedBytes += msgLength;
+                    receivedFrames++;
 
-        long ms;
-        long diff = currentTime.tv_nsec - lastTime.tv_nsec;
-        ms = diff / (long)1.0e6;
+                    if (index == 0) {
+                        startTime = getTimeMs();
+                    } else if (index == 99) {  // 100 frames received
+                        sendModeEndRequest(ackDown);
+                        sendData = false;
+                        long diffTime = getTimeMs() - startTime;
+                        DSTATUS("MOC - Ack down test ended : %ld frames received (%ld Bytes) in %ld ms", receivedFrames,
+                                receivedBytes, diffTime);
+                        double dataFlow = receivedBytes * 1000 / diffTime;
+                        DSTATUS("MOC - Ack down flow = %lf Bytes/s", dataFlow);
+                        testMode = none;
+                    }
 
-        lastTime = currentTime;
-
-        DSTATUS("Diff ms = %lu", diff);
-        DSTATUS("MOC - Data received : %u", msgLength);
+                    // Send next frame request
+                    if (sendData) {
+                        index++;
+                        uint8_t b[2];
+                        b[0] = '-'; // random char to avoid START_CHAR or END_CHAR
+                        b[1] = index;
+                        DSTATUS("MOC - Ack down test : Send request %u", index);
+                        vehicle->moc->sendDataToMSDK(b, 100);
+                    }
+                }
+                    break;
+                case none:
+                    // Data received displayed as string
+                    if(msgLength == 100) {
+                        msgLength = 99;
+                    }
+                    data[msgLength] = '\0';
+                    DSTATUS("MOC - Data received (%u) : %s", msgLength, data);
+                    displayMenu();
+                    break;
+            }
+            break;
     }
 }
 
-void
-sendData()
-{
-    char c[] = "Hello world from M210Pi3";
-    DSTATUS("Send data to mobile: %s", c);
-    vehicle->moc->sendDataToMSDK(reinterpret_cast<uint8_t*>(&c), sizeof(c));
+long getTimeMs() {
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+    long ms = (long) tp.tv_sec * 1000L + tp.tv_usec / 1000; //get current timestamp in milliseconds
+    return ms;
+}
+
+void sendModeStartRequest(MocTestMode testMode) {
+    sendModeRequest(START_CHAR, testMode);
+
+}
+
+void sendModeEndRequest(MocTestMode testMode) {
+    sendModeRequest(END_CHAR, testMode);
+}
+
+void sendModeRequest(char operation, MocTestMode testMode) {
+    char initFrame[2];
+    initFrame[0] = operation;
+    initFrame[1] = testMode;
+    vehicle->moc->sendDataToMSDK(reinterpret_cast<uint8_t*>(&initFrame), sizeof(initFrame));
 }
