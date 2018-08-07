3a68ea0aba61bdea91346d7d7675fea8136c1060
Jonathan Michel <jonathanmichel@bluewin.ch>
2018-08-07 16:36:37 +0200

    ActionData macro implementation for push/pop methods

--- Action/ActionData.cpp
+++ Action/ActionData.cpp
@@ -12,161 +12,107 @@
 
 using namespace M210;
 
-// TODO Find a way to do pop with RTTI (Templates)
-
 pthread_mutex_t ActionData::mutex = PTHREAD_MUTEX_INITIALIZER;
 
 ActionData::ActionData(ActionId actionId, size_t size) {
     this->actionId = actionId;
     this->dataSize = size;
     this->dataPosCnt = 0;
-    this->data = nullptr;
+    this->dataPtr = nullptr;
     if(size > 0)
-        data = new char[size];
+        dataPtr = new char[size];
 }
 
-
 ActionData::~ActionData() {
-    delete[] data;
+    delete[] dataPtr;
 }
 
 bool ActionData::checkSize(size_t size) const{
-    return (dataPosCnt + size <= this->dataSize);
-}
-
-bool ActionData::_push(const char *c, size_t length) {
-    pthread_mutex_lock(&mutex);
-    if(!checkSize(length)) {
-        DERROR("Unable to push data");
-        pthread_mutex_unlock(&mutex);
-        return false;
-    }
-    memcpy(data + dataPosCnt, c, length);
-    dataPosCnt += length;
-    pthread_mutex_unlock(&mutex);
-    return true;
+    return (dataPosCnt + size <= dataSize);
 }
 
 bool ActionData::push(char c) {
-    return _push(&c, sizeof(char));
+    _push(&c, char);
 }
 
 bool ActionData::push(int i) {
-    return _push(reinterpret_cast<const char *>(&i), sizeof(int));
+    _push(&i, int);
 }
 
 bool ActionData::push(unsigned u) {
-    return _push(reinterpret_cast<const char *>(&u), sizeof(unsigned));
+    _push(&u, unsigned);
 }
 
 bool ActionData::push(float f) {
-    return _push(reinterpret_cast<const char *>(&f), sizeof(float));
+    _push(&f, float);
 }
 
 bool ActionData::push(const Telemetry::Vector3f &v) {
-    return _push(reinterpret_cast<const char *>(&v), sizeof(Telemetry::Vector3f));
+    _push(&v, Telemetry::Vector3f);
 }
 
 bool ActionData::push(const uint8_t *data, size_t length) {
-    return _push(reinterpret_cast<const char *>(data), length);
+    __push(data, length);
 }
 
-
 bool ActionData::popChar(char &c) {
-    pthread_mutex_lock(&mutex);
-    size_t length = sizeof(char);
-    if(dataPosCnt < length) {
-        DERROR("Unable to pop char");
-        pthread_mutex_unlock(&mutex);
-        return false;
-    }
-    dataPosCnt -= length;
-    memcpy(&c, data + dataPosCnt, length);
-    pthread_mutex_unlock(&mutex);
-    return true;
+    _pop(c, char);
 }
 
 bool ActionData::popInt(int &i) {
-    pthread_mutex_lock(&mutex);
-    size_t length = sizeof(int);
-    if(dataPosCnt < length) {
-        DERROR("Unable to pop int");
-        pthread_mutex_unlock(&mutex);
-        return false;
-    }
-    dataPosCnt -= length;
-    memcpy(&i, data + dataPosCnt, length);
-    pthread_mutex_unlock(&mutex);
-    return true;
+    _pop(i, int);
 }
 
 bool ActionData::popUnsigned(unsigned &u) {
-    pthread_mutex_lock(&mutex);
-    size_t length = sizeof(unsigned);
-    if(dataPosCnt < length) {
-        DERROR("Unable to pop unsigned");
-        pthread_mutex_unlock(&mutex);
-        return false;
-    }
-    dataPosCnt -= length;
-    memcpy(&u, data + dataPosCnt, length);
-    pthread_mutex_unlock(&mutex);
-    return true;
+    _pop(u, unsigned);
 }
 
 bool ActionData::popFloat(float &f) {
-    pthread_mutex_lock(&mutex);
-    size_t length = sizeof(float);
-    if(dataPosCnt < length) {
-        DERROR("Unable to pop float");
-        pthread_mutex_unlock(&mutex);
-        return false;
-    }
-    dataPosCnt -= length;
-    memcpy(&f, data + dataPosCnt, length);
-    pthread_mutex_unlock(&mutex);
-    return true;}
+    _pop(f, float);
+}
 
 bool ActionData::popVector3f(Telemetry::Vector3f &v) {
-    pthread_mutex_lock(&mutex);
-    size_t length = sizeof(Telemetry::Vector3f);
-    if(dataPosCnt < length) {
-        DERROR("Unable to pop Vector3f");
-        pthread_mutex_unlock(&mutex);
-        return false;
-    }
-    dataPosCnt -= length;
-    memcpy(&v, data + dataPosCnt, length);
-    pthread_mutex_unlock(&mutex);
-    return true;
+    _pop(v, Telemetry::Vector3f);
 }
 
 void ActionData::unitTest() {
-    // Todo test all type
-    uint8_t i = 17;
+    char c = 'k';
+    int i = -25178;
+    unsigned u = 5891;
     float x = 2.52;
     float y = 5.17;
     float z = 78.35;
     float yaw = 45.25;
 
+    M210::ActionData::ActionId id = M210::ActionData::ActionId::stopAircraft;
+    // Allocate dynamic memory
+    ActionData ad(id, sizeof(uint8_t) + sizeof(float) * 4);
+    ad.push(c);
+    /*/ Push float array
     auto ch = new char[16];
     memcpy(ch, reinterpret_cast<char*>(&x), 4);
     memcpy(ch+4, reinterpret_cast<char*>(&y), 4);
     memcpy(ch+8, reinterpret_cast<char*>(&z), 4);
     memcpy(ch+12, reinterpret_cast<char*>(&yaw), 4);
+    ad.push(reinterpret_cast<uint8_t*>(ch), 16);
+    //*/
 
-    ActionData ad(M210::ActionData::stopAircraft, 17);
-    ad.push((char)i);
-    // Push float array
-    // ad.push(reinterpret_cast<uint8_t*>(ch), 16);
     // Push float 1 by 1
     ad.push(x);
     ad.push(y);
     ad.push(z);
     ad.push(yaw);
-     //*/
+    //*/
 
-    uint8_t i2 = 0;
+    // Try to push i, has to return false
+    // because memory allocation was to small
+    bool push = ad.push(i);
+
+    // Try to get all data back
+    // /!\ Lifo !
+    M210::ActionData::ActionId id2 = ad.getActionId();
+    int i2 = 0;
+    unsigned u2 = 0;
     char c2 = 0;
     float x2 = 0, y2 = 0, z2 = 0, yaw2 = 0;
     Telemetry::Vector3f v2;
@@ -184,8 +130,22 @@ void ActionData::unitTest() {
 
     ad.popChar(c2);
 
-    i2 = (uint8_t)c2;
+    // Push and instant pop data
+    ad.push(i);
+    ad.push(u);
 
+    ad.popUnsigned(u2);
+    ad.popInt(i2);
+
+    // Try to pop char, has to return false
+    // because all data have been popped
+    char c3;
+    bool pop = ad.popChar(c3);
+    //*/
+
+    // Verify that all data recovered are identical to initials ones
+    //assert(!push);
+    assert(id == id2);
     assert(yaw == yaw2);
     assert(z == z2);
     assert(y == y2);
@@ -193,7 +153,11 @@ void ActionData::unitTest() {
     assert(x == v2.x);
     assert(y == v2.y);
     assert(z == v2.z);
+    assert(c == c2);
+    assert(u == u2);
     assert(i == i2);
+    assert(!pop);
 
     DSTATUS("ActionData test passed");
+    DERROR("Please do not pay attention to the last two errors if ActionData test passed");
 }
--- Action/ActionData.h
+++ Action/ActionData.h
@@ -2,6 +2,16 @@
  *  @version 1.0
  *  @date Jul 20 2018
  *  @author Jonathan Michel
+ *  @brief ActionData objects are added in Action queue (Action.h)
+ *  The goal is to provide an object with variable data size.
+ *  It is useful because all actions doesn't need the same amount of
+ *  data.
+ *  On object creation, dynamic memory is allocated. Size depends of user
+ *  need. Data can next be pushed (not all type are yet supported,
+ *  only main ones). Then, data can be recovered with pop method.
+ *  /!\ Push/Pop methods works as a lifo, last pushed value will be first
+ *  popped
+ *  Example of use in unitTest() method
  */
 
 #ifndef MATRICE210_ACTIONDATA_H
@@ -11,11 +21,67 @@
 
 #include <dji_vehicle.hpp>
 
+/**
+ * Read data from dynamic memory
+ * @param _data_ Pointer to data where write result
+ * @param _type_ Type of data to read
+ * @return False if there is no more data to read in allocated dynamic memory, true otherwise
+ */
+#define _pop(_data_, _type_)                        \
+{                                                   \
+    pthread_mutex_lock(&mutex);                     \
+    size_t length = sizeof(_type_);                 \
+    if(dataPosCnt < length) {                       \
+        DERROR("Unable to pop %s", #_type_);        \
+        pthread_mutex_unlock(&mutex);               \
+        return false;                               \
+    }                                               \
+    dataPosCnt -= length;                           \
+    memcpy(&_data_, dataPtr + dataPosCnt, length);  \
+    pthread_mutex_unlock(&mutex);                   \
+    return true;                                    \
+}
+
+/**
+ * Copy data to allocated dynamic memory
+ * @param _data_ Pointer to data to copy
+ * @param _length_ Length of data to copy [bytes]
+ * @return False if there is not enough memory allocated, true otherwise
+ */
+#define __push(_data_, _length_)                        \
+{                                                       \
+    pthread_mutex_lock(&mutex);                         \
+    auto ptr = reinterpret_cast<const char *>(_data_);  \
+    if(!checkSize(_length_)) {                          \
+        DERROR("Unable to push data");                  \
+        pthread_mutex_unlock(&mutex);                   \
+        return false;                                   \
+    }                                                   \
+    memcpy(dataPtr + dataPosCnt, ptr, _length_);        \
+    dataPosCnt += _length_;                             \
+    pthread_mutex_unlock(&mutex);                       \
+    return true;                                        \
+}
+
+/**
+ * Copy data to allocated dynamic memory
+ * @param _data_ Pointer to data to copy
+ * @param _type_ Type of data to copy
+ * @return False if there is not enough memory allocated, true otherwise
+ */
+#define _push(_data_, _type_)           \
+{                                       \
+    __push(_data_, sizeof(_type_));     \
+}
+
 using namespace DJI::OSDK;
 
 namespace M210 {
     class ActionData {
     public:
+        // Action id indicates which action is relative to
+        // current action data object
+        // todo move this declaration in a better place
         enum ActionId {
             takeOff,
             landing,
@@ -28,49 +94,64 @@ namespace M210 {
             helloWorld
         };
     private:
-        char *data;
-        size_t dataPosCnt;
-        size_t dataSize;
-        ActionId actionId;
+        char *dataPtr;      /*!< Pointer to dynamic memory allocated */
+        size_t dataPosCnt;  /*!< Current numbers of bytes used */
+        size_t dataSize;    /*!< Size allocated in dynamic memory [bytes] */
+        ActionId actionId;  /*!< Action id concerned by current action data */
 
-        bool _push(const char *c, size_t length);
-
+        /**
+         * Check if enough place has been allocated to copy data
+         * @param size Size of data to copy [bytes]
+         * @return True if enough place is available
+         */
         bool checkSize(size_t size) const;
 
         // Mutex
         static pthread_mutex_t mutex;
     public:
+        /**
+         * Allocates dynamic memory for specified action
+         * @param actionId Action id concerned by current action data
+         * @param size Size to allocate in dynamic memory [bytes]
+         */
         explicit ActionData(ActionId actionId, size_t size = 0);
 
         ~ActionData();
 
+        ActionId getActionId() const { return actionId; }
+
+        /**
+         * Unit test to check that class is working. Called at the
+         * beginning of the program. Assert if a test fails
+         */
         static void unitTest();
 
+        // todo find a way to implement all push methods with RTTI (Templates?)
+        /**
+         * Dedicated push methods
+         * See _push() macro for details
+         * @param data to push
+         * @return False if there is not enough memory allocated, true otherwise
+         */
         bool push(char c);
-
         bool push(int i);
-
         bool push(unsigned u);
-
         bool push(float f);
-
         bool push(const Telemetry::Vector3f &v);
-
         bool push(const uint8_t *data, size_t length);
 
+        // todo replace implementation with macro
+        /**
+         * Dedicated pop methods
+         * See _pop() macro for details
+         * @param return variable
+         * @return False if there is no more data to read, true otherwise
+         */
         bool popChar(char &c);
-
         bool popInt(int &i);
-
         bool popUnsigned(unsigned &u);
-
         bool popFloat(float &f);
-
         bool popVector3f(Telemetry::Vector3f &v);
-
-        void setAction(ActionId actionId1) { this->actionId = actionId1; }
-
-        ActionId getActionId() const { return actionId; }
     };
 }
 
